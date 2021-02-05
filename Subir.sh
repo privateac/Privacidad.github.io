9411facf557f7e3ac7282d359e3781a1464f18d5
privateac <privateaccounts32@tutanota.com>
2021-02-04 23:00:16 -0600

    Prueba 1

---
+++ Subir.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+git add --all
+echo ""
+echo "git add -all ,complete"
+echo ""
+read -p "What comment would you want??" ANS
+git commit -m "$ANS"
+echo ""
+echo "Commit with comment added"
+echo ""
+git push -u origin main
+echo ""
+echo "git push -u origin main, completed :)"
